de48a35216287be24da9c13538e7ef75949e1400
Sean Blessing <snblessing@gmail.com>
2021-04-27 16:07:37 -0400

    thru ch6

--- ch05-class-demos/src/ExceptionHandlerApp.java
+++ ch05-class-demos/src/ExceptionHandlerApp.java
@@ -8,10 +8,22 @@ public class ExceptionHandlerApp {
 		
 		// prompt user for a whole number
 		Scanner sc = new Scanner(System.in);
+
+		int nbr = getInt("Enter a whole number: ", sc);
+		System.out.println("Good job!  Number entered is: "+nbr);
+
+		double dbl = getDouble("Enter a double number: ", sc);
+		System.out.println("Good job!  Number entered is: "+dbl);
+		
+		System.out.println("Bye");
+
+	}
+
+	private static int getInt(String prompt, Scanner sc) {
 		boolean isValid = false;
 		int n = 0;
 		while (!isValid) {
-			System.out.print("Enter a whole number: ");
+			System.out.print(prompt);
 			try {
 				n = sc.nextInt();
 				isValid = true;
@@ -21,9 +33,23 @@ public class ExceptionHandlerApp {
 				sc.nextLine();
 			}			
 		}
-		System.out.println("Good job!  Number entered is: "+n);
-		System.out.println("Bye");
-
+		return n;
 	}
 
+	private static double getDouble(String prompt, Scanner sc) {
+		boolean isValid = false;
+		double d = 0.0;
+		while (!isValid) {
+			System.out.print(prompt);
+			if (sc.hasNextDouble()) {
+				d = sc.nextDouble();
+				isValid = true;
+			}
+			else {
+				System.out.println("Invalid input.");
+				sc.next();
+			}			
+		}
+		return d;
+	}
 }
---
+++ ch06-class-demos/src/DebuggingApp.java
@@ -0,0 +1,11 @@
+
+public class DebuggingApp {
+
+	public static void main(String[] args) {
+		//p.183
+		double d = .2 + .7;
+		System.out.println(d);
+
+	}
+
+}
