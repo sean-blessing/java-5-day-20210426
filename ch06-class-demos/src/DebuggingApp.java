eda203d5c5275d460d906b3ba9870790fe03b043
Sean Blessing <snblessing@gmail.com>
2021-04-29 20:53:20 -0400

    added debugging tests for next class

--- ch06-class-demos/src/DebuggingApp.java
+++ ch06-class-demos/src/DebuggingApp.java
@@ -5,7 +5,12 @@ public class DebuggingApp {
 		//p.183
 		double d = .2 + .7;
 		System.out.println(d);
-
+		//String str = null;
+		//System.out.println("str = "+str.toString());
+		for (int i=0; i<10; i++) {
+			System.out.println("hello "+i);
+		}
+		System.out.println("End");
 	}
 
 }
